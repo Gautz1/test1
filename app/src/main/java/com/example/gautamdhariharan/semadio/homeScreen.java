ff32e93a31d583d21c9dda16768a8d7bda1beab5
nareinrao <jabbawock004@gmail.com>
2018-01-16 21:21:18 +0530

    Provided changes to home screen wrt orientation.

--- app/src/main/java/com/example/gautamdhariharan/semadio/homeScreen.java
+++ app/src/main/java/com/example/gautamdhariharan/semadio/homeScreen.java
@@ -1,6 +1,7 @@
 package com.example.gautamdhariharan.semadio;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -29,35 +30,42 @@ public class homeScreen extends AppCompatActivity {
         ScreenParam sp = new ScreenParam();
         int width = sp.getScreenWidth(homeScreen.this);int height = sp.getScreenHeight(homeScreen.this);
         TextView text;
+        int height_ad,height_vid;
+        if(this.getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT){
+            height_ad=(int)(height/2.5);height_vid=height/3;
+        }
+        else{
+            height_ad=height;height_vid=(int)(height/1.15);
+        }
         ll=(LinearLayout)findViewById(R.id.adll);
         for(int j=0;j<ll.getChildCount();j++){
             text = (TextView)ll.getChildAt(j);
-            text.setWidth(width);text.setHeight((int)(height/2.5));
+            text.setWidth(width);text.setHeight(height_ad);
         }
         ll=(LinearLayout)findViewById(R.id.engll);
         for(int j=0;j<ll.getChildCount();j++){
             text = (TextView)ll.getChildAt(j);
-            text.setWidth(width);text.setHeight(height/3);
+            text.setWidth(width);text.setHeight(height_vid);
         }
         ll=(LinearLayout)findViewById(R.id.hinll);
         for(int j=0;j<ll.getChildCount();j++){
             text = (TextView)ll.getChildAt(j);
-            text.setWidth(width);text.setHeight(height/3);
+            text.setWidth(width);text.setHeight(height_vid);
         }
         ll=(LinearLayout)findViewById(R.id.tamll);
         for(int j=0;j<ll.getChildCount();j++){
             text = (TextView)ll.getChildAt(j);
-            text.setWidth(width);text.setHeight(height/3);
+            text.setWidth(width);text.setHeight(height_vid);
         }
         ll=(LinearLayout)findViewById(R.id.mpll);
         for(int j=0;j<ll.getChildCount();j++){
             text = (TextView)ll.getChildAt(j);
-            text.setWidth(width);text.setHeight(height/3);
+            text.setWidth(width);text.setHeight(height_vid);
         }
         ll=(LinearLayout)findViewById(R.id.telll);
         for(int j=0;j<ll.getChildCount();j++){
             text = (TextView)ll.getChildAt(j);
-            text.setWidth(width);text.setHeight(height/3);
+            text.setWidth(width);text.setHeight(height_vid);
         }
         text = (TextView)findViewById(R.id.mostviewed);
         text.setHeight(height/5);
